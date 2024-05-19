57ffc2d837fc8fc2809e44ad22b75fdddf140d2c
Werick Nalyson <wericknalyson@gmail.com>
2024-05-19 13:37:59 -0300

    feat: create search params and result

--- src/shared/domain/repository/repository-interface.ts
+++ src/shared/domain/repository/repository-interface.ts
@@ -1,5 +1,7 @@
 import { Entity } from "../entity";
 import { ValueObject } from "../value-object";
+import { SearchParams } from "./search-params";
+import { SearchResult } from "./search-result";
 
 export interface IRepository<E extends Entity, EntityId extends ValueObject> {
   insert(entity: E): Promise<void>;
@@ -11,4 +13,15 @@ export interface IRepository<E extends Entity, EntityId extends ValueObject> {
   findAll(): Promise<E[]>;
 
   getEntity(): new (...args: any[]) => E;
+}
+
+export interface ISearchableRepository<
+  E extends Entity,
+  EntityId extends ValueObject,
+  Filter = string,
+  SearchInput = SearchParams<Filter>,
+  SearchOutput = SearchResult,
+> extends IRepository<E, EntityId> {
+  sortableFields: string[];
+  search(props: SearchInput): Promise<SearchOutput>;
 }
