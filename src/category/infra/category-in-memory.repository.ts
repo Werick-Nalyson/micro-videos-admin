02478e5bb8938873be11080182c3211b1e5765c1
Werick Nalyson <wericknalyson@gmail.com>
2024-05-19 14:51:05 -0300

    test: testing category in-memory

--- src/category/infra/category-in-memory.repository.ts
+++
@@ -1,9 +0,0 @@
-import { Uuid } from "../../shared/domain/value-objects/uuid.vo";
-import { InMemoryRepository } from "../../shared/infra/db/in-memory/in-memory.repository";
-import { Category } from "../domain/category.entity";
-
-export class CategoryInMemoryRepository extends InMemoryRepository<Category, Uuid> {
-  getEntity(): new (...args: any[]) => Category {
-    return Category
-  }
-}
---
+++ src/category/infra/db/category-in-memory.repository.ts
@@ -0,0 +1,35 @@
+import { SortDirection } from "../../../shared/domain/repository/search-params";
+import { Uuid } from "../../../shared/domain/value-objects/uuid.vo";
+import { InMemorySearchableRepository } from "../../../shared/infra/db/in-memory/in-memory.repository";
+import { Category } from "../../domain/category.entity";
+
+export class CategoryInMemoryRepository extends InMemorySearchableRepository<Category, Uuid> {
+  sortableFields: string[] = ['name', 'created_at'];
+
+  getEntity(): new (...args: any[]) => Category {
+    return Category
+  }
+
+  protected async applyFilter(
+    items: Category[],
+    filter: string,
+  ): Promise<Category[]> {
+    if (!filter) {
+      return items;
+    }
+
+    return items.filter((i) => {
+      return i.name.toLowerCase().includes(filter.toLowerCase());
+    });
+  }
+
+  protected applySort(
+    items: Category[],
+    sort: string | null,
+    sort_dir: SortDirection | null,
+  ) {
+    return sort
+      ? super.applySort(items, sort, sort_dir)
+      : super.applySort(items, 'created_at', 'desc');
+  }
+}
