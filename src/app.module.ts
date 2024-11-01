ed27ccc6415c48680badeb84e2ecd570fc6f6e62
Werick Nalyson <wericknalyson@gmail.com>
2024-11-01 00:16:06 -0300

    style: rename and move nestjs modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { CategoriesModule } from './categories/categories.module';
-import { getModelToken } from '@nestjs/sequelize';
-import { CategoryModel } from '@core/category/infra/db/sequelize/category.model';
-import { CategorySequelizeRepository } from '@core/category/infra/db/sequelize/category-sequelize.repository';
-import { DatabaseModule } from './database/database.module';
-import { ConfigModule } from './config/config.module';
+import { ConfigModule } from './nest-modules/config-module/config.module';
+import { DatabaseModule } from './nest-modules/database-module/database.module';
+import { CategoriesModule } from './nest-modules/categories-module/categories.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), DatabaseModule, CategoriesModule],
-  controllers: [AppController],
-  providers: [
-    AppService,
-    {
-      provide: CategorySequelizeRepository,
-      useFactory: (categoryModel: typeof CategoryModel) =>
-        new CategorySequelizeRepository(categoryModel),
-      inject: [getModelToken(CategoryModel)],
-    },
-  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/nest-modules/categories-module/categories.controller.spec.ts
+++ src/nest-modules/categories-module/categories.controller.spec.ts
@@ -1,8 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CategoriesController } from './categories.controller';
-import { DatabaseModule } from '../database/database.module';
+import { DatabaseModule } from '../database-module/database.module';
 import { CategoriesModule } from './categories.module';
-import { ConfigModule } from '../config/config.module';
+import { ConfigModule } from '../config-module/config.module';
 
 describe('CategoriesController', () => {
   let controller: CategoriesController;
