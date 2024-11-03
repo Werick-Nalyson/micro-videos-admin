7688f349979ddb155b37845dfc9aaf2fb804248c
Werick Nalyson <wericknalyson@gmail.com>
2024-11-03 12:08:17 -0300

    feat: delete category controller

--- src/nest-modules/categories-module/categories.controller.ts
+++ src/nest-modules/categories-module/categories.controller.ts
@@ -8,6 +8,7 @@ import {
   Delete,
   Inject,
   ParseUUIDPipe,
+  HttpCode,
 } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
@@ -62,8 +63,13 @@ export class CategoriesController {
     return CategoriesController.serialize(output);
   }
 
+  @HttpCode(204)
   @Delete(':id')
-  remove(@Param('id') id: string) {}
+  async remove(
+    @Param('id', new ParseUUIDPipe({ errorHttpStatusCode: 422 })) id: string,
+  ) {
+    return this.deleteCategory.execute({ id });
+  }
 
   static serialize(output: CategoryOutput) {
     return new CategoryPresenter(output);
