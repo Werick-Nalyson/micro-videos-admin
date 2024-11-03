eb010c72117cf3470e2143e35d640aa03c92e07f
Werick Nalyson <wericknalyson@gmail.com>
2024-11-03 12:10:28 -0300

    feat: get category controller

--- src/nest-modules/categories-module/categories.controller.ts
+++ src/nest-modules/categories-module/categories.controller.ts
@@ -48,7 +48,13 @@ export class CategoriesController {
   findAll() {}
 
   @Get(':id')
-  findOne(@Param('id') id: string) {}
+  async findOne(
+    @Param('id', new ParseUUIDPipe({ errorHttpStatusCode: 422 })) id: string,
+  ) {
+    const output = await this.getCategory.execute({ id });
+
+    return CategoriesController.serialize(output);
+  }
 
   @Patch(':id')
   async update(
